515ef01890c3a5014c058edff66213e6572fd736
Tony Xia <xia_tony@hotmail.com>
2018-10-07 17:46:24 +1100

    Post a message to Teams channel when cosmos db gets a new document.

--- CosmosDB2Teams/SendFeedbackFunction.cs
+++ CosmosDB2Teams/SendFeedbackFunction.cs
@@ -1,15 +1,20 @@
 using System.Collections.Generic;
+using System.Configuration;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace CosmosDB2Teams
 {
     public static class SendFeedbackFunction
     {
         [FunctionName("SendFeedbackToTeams")]
-        public static void Run(
+        public static async Task Run(
             [CosmosDBTrigger(
             databaseName: "demo",
             collectionName: "userfeedback",
@@ -19,8 +24,28 @@ namespace CosmosDB2Teams
         {
             if (input != null && input.Count > 0)
             {
-                log.LogInformation("Documents modified " + input.Count);
-                log.LogInformation("First document Id " + input[0].Id);
+                var document = input[0];
+
+                log.LogInformation("New document Id " + document.Id);
+
+                var customerName = document.GetPropertyValue<string>("customerName");
+                var customerPhone = document.GetPropertyValue<string>("customerPhone");
+                var category = document.GetPropertyValue<string>("category");
+                var description = document.GetPropertyValue<string>("description");
+                await PostMessageToTeams(customerName, customerPhone, category, description);
+
+                log.LogInformation("Post a message to teams successfully.");
+            }
+        }
+
+        private static async Task PostMessageToTeams(string customerName, string customerPhone, string category, string description)
+        {
+            var teamsChannelUrl = ConfigurationManager.AppSettings["TeamsChannelUrl"];
+            var payload = JsonConvert.SerializeObject(new { text = customerName });
+            using (var client = new HttpClient())
+            {
+                var content = new StringContent(payload, Encoding.UTF8, "application/json");
+                await client.PostAsync(teamsChannelUrl, content);
             }
         }
     }
